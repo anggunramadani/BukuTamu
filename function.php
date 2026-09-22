52b3bb36c9a825f0b299d99357f18bff2aa2b32d
anggunramadani0814.dotcom <anggunramadani081@gmail.com>
2026-09-23 02:27:33 +0700

    menambahkan halaman buku tamu dan fitur menampilkan data

---
+++ function.php
@@ -0,0 +1,16 @@
+<?php
+//panggil file koneksi.php
+
+require_once('koneksi.php');
+
+//membuat query ke /  dari database 
+function query(string $query)
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
--- templates/header.php
+++ templates/header.php
@@ -9,16 +9,19 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <!-- Custom styles for this page -->
+    <link href="../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <title>SB Admin 2 - Blank</title>
 
     <!-- Custom fonts for this template-->
-    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -136,7 +139,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                                 <img class="assets/img/undraw_profile.svg"
-                                    src=" img/undraw_profile.svg">
+                                    src=" ../assets/img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
