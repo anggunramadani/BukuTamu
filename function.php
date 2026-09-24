fdaa4a74f35fc4546cae2949c4fa97bac364f06b
anggunramadani0814.dotcom <anggunramadani081@gmail.com>
2026-09-24 22:41:47 +0700

    membuat fitur hapus data user dengan confirmasi

--- function.php
+++ function.php
@@ -87,3 +87,14 @@ function tambah_user($data)
 
     return mysqli_affected_rows($koneksi);
 }
+
+function hapus_user($id)
+{
+    global $koneksi;
+
+    $query = "DELETE FROM users WHERE id_user = '$id' ";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
